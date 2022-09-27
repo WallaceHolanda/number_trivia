c27f9a64c9955bdb91c6f7ca72441c678ee09e8b
WallaceHolanda <wallace.holanda@hotmail.com>
2022-09-27 14:35:46 -0300

    feat: adiciona o arquivo de estados do bloc

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors_in_immutables
+
+part of 'number_trivia_bloc.dart';
+
+abstract class NumberTriviaState extends Equatable {
+  const NumberTriviaState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  Loaded({required this.trivia});
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({required this.message});
+}
