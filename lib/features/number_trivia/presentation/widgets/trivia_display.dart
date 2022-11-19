2693710415e4f77fb3b39da8f82274a613e787f3
WallaceDHolanda <wallace.holanda@popcode.com.br>
2022-11-18 21:30:32 -0300

    feat: cria widget do number trivia e mensagem

---
+++ lib/features/number_trivia/presentation/widgets/trivia_display.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+
+class TriviaDisplay extends StatelessWidget {
+  final NumberTrivia numberTrivia;
+
+  const TriviaDisplay({
+    super.key,
+    required this.numberTrivia,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    return SizedBox(
+      height: screenHeight / 3,
+      child: Column(
+        children: [
+          Text(
+            numberTrivia.number.toString(),
+            style: const TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: SingleChildScrollView(
+                child: Text(
+                  numberTrivia.text,
+                  style: const TextStyle(fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
